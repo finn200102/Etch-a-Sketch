2791648f5282714fafd218eb2562f809a0790c83
Finn <112250336+finn200102@users.noreply.github.com>
2024-10-14 16:33:10 +0200

    sketching now works with 10 opacity levels

--- sketch-engine.js
+++ sketch-engine.js
@@ -2,12 +2,14 @@ function buttonClick(numberPerSide = 16) {
   const sketchBoard = document.querySelector("#sketch-board");
   for (let i = 0; i < numberPerSide ** 2; i++) {
     const div = document.createElement("div");
-    div.style.backgroundColor = "blue";
+    div.style.backgroundColor = "white";
+    div.style.opacity = "1";
     let percent = (1 / numberPerSide) * 100;
     div.style.width = percent.toString() + "%";
     // div.style.border = "2px solid black";
     div.addEventListener("mouseover", () => {
-      div.style.backgroundColor = "green";
+      //div.style.backgroundColor = "black";
+      div.style.opacity = (Number(div.style.opacity) - 0.1).toString();
     });
 
     div.classList.add("pixel");
@@ -16,7 +18,17 @@ function buttonClick(numberPerSide = 16) {
 }
 
 function showPopup() {
-  let number = prompt("Please enter number:");
+  let number = Number(prompt("Please enter number:"));
+  if (!Number.isInteger(number)) {
+    return false;
+  }
+  if (number > 100) {
+    // Set the number to 100 if too big
+    number = 100;
+  }
+  if (number < 1) {
+    number = 1;
+  }
   clearField();
   buttonClick((numberPerSide = number));
 }
